bcaafc9e28c395b7ca212adc3c3e33a6b9d24ce5
cyberbhoi <111127802+cyberbhoi@users.noreply.github.com>
2024-02-04 14:58:08 +0530

    coming soon status added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
 import * as React from "react"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from "react-bootstrap";
 import { Container } from "react-bootstrap";
+import Spinner from "react-bootstrap/Spinner";
 
 
 const Home = () =>{
 
   return(
     <Container>
-        <h1>Cyberbhoi is Awesome.</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus, reprehenderit error dolor itaque totam modi in aliquid at nam, quam neque! Expedita, a perferendis! Praesentium provident itaque dolore minima aperiam!</p>
-        <Button variant="primary">Start Coding</Button>
+        <h1> Coming Soon...  <Spinner animation="border" variant="primary" />
+</h1>
+       <h1>Website is under construction </h1>
     </Container>
   )
 }
